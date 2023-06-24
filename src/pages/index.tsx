7b3f739b3cab9f6b6d934cbaf28f44cc25ec472f
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-06-24 15:58:28 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import React from "react";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
             <div
                 className="relative flex place-items-center">
                 <Image
