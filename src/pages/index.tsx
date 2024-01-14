8bf24bed45be94ba4261c2e81c2f4516b9264b4b
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-01-14 12:53:44 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,11 +19,11 @@ export default function Home() {
 
             <div className="text-xl mb-16 grid text-center">
                 <p>Hello, I&apos;m Joakim Taule Kartveit</p>
-                <ul className="max-w-md space-y-1 list-none list-inside">
+                <ul className="max-w-md list-none list-inside">
                     <li>I&apos;m a dad and husband</li>
                     <li>I&apos;m working as a developer at
                         <a className="font-medium hover:underline space-x-1" href="https://nav.no"
-                        >NAV</a> <br/></li>
+                        > NAV</a> <br/></li>
                     <li>I&apos;m based in Norway, Kristiansand.</li>
                     <li>I like to play soccer sometimes</li>
                     <li>Current OS is Linux</li>
