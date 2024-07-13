3f95895f5d501675b2f6876e8ecf640ab9d2dfde
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-07-13 18:41:20 +0200

    Update projects.tsx

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -22,8 +22,8 @@ const Projects: NextPage = () => {
                         />
                     </>
                 ))}
-                <p>The rest of mye projects can be found on my: &nbsp;
-                    <a href={"https://github.com/MikAoJk?tab=repositories"} className="font-medium hover:underline">
+                <p>The rest of mye projects can be found here:
+                    <a href={"https://github.com/MikAoJk?tab=repositories"} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                         {"GitHub repositories"}
                     </a>
                 </p>
