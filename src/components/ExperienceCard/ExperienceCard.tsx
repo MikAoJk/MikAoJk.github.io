b91a00f5e2e72bc10fa556543392a26ca171326b
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-10-02 09:05:42 +0200

    chore: added in diffrent hover colour on experiencecard

--- src/components/ExperienceCard/ExperienceCard.tsx
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -14,7 +14,7 @@ export const ExperienceCard: React.FC<ExperienceCardProps> = ({title, company, f
         <div>
             <a href={companyLink}>
                 <div
-                    className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10">
+                    className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 hover:bg-gray-700">
                     <h1 className="font-semibold text-xl">{title}</h1>
                     <p className="text-gray-600 dark:text-gray-400 my-2">Period: {fromDate} - {toDate}</p>
                     {company}
