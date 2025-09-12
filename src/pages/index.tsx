02475d36cbefd2d869c4efabec0fdb6ab77307c8
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-09-12 17:52:30 +0200

    Replace personal info with InfoAboutMeCard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import Footer from "@/components/Footer/Footer";
+import { InfoAboutMeCard } from "@/components/InfoAboutMeCard/InfoAboutMeCard";
+
 import React from "react";
 
 export default function Home() {
@@ -21,23 +23,28 @@ export default function Home() {
             </div>
             
 
-            <div className="text-xl mb-16 grid text-center mt-10">
-                <p>Hello, I&apos;m Joakim Taule Kartveit</p>
-                <ul className="max-w-md list-none list-inside">
-                    <li>A dad and husband</li>
-                    <li>Working as a developer at
-                        <a className="font-medium hover:underline space-x-1" href="https://nav.no"
-                        > NAV</a> <br/></li>
-                    <li>I&apos;m based in Norway, Kristiansand.</li>
-                    <li>I like to play soccer sometimes</li>
-                    <li>Current OS is Ubuntu</li>
-                    <li>Love Open Source Software</li>
-                    <li>How to reach me: {" "}
-                        <a className="text-blue-600 dark:text-blue-500 hover:underline"
-                           href={`mailto:joakimkartveit@gmail.com`}
-                        >joakimkartveit@gmail.com </a></li>
-                </ul>
-            </div>
+            <InfoAboutMeCard
+                name="Joakim Taule Kartveit"
+                location="Norway, Kristiansand"
+                tagline="Developer at NAV"
+                email="joakimkartveit@gmail.com"
+                details={
+                    <ul className="max-w-md list-none list-inside">
+                        <li>A dad and husband</li>
+                        <li>Working as a developer at
+                            <a className="font-medium hover:underline space-x-1" href="https://nav.no"
+                            > NAV</a> <br/></li>
+                        <li>I&apos;m based in Norway, Kristiansand.</li>
+                        <li>I like to play soccer sometimes</li>
+                        <li>Current OS is Ubuntu</li>
+                        <li>Love Open Source Software</li>
+                        <li>How to reach me: {" "}
+                            <a className="text-blue-600 dark:text-blue-500 hover:underline"
+                               href={`mailto:joakimkartveit@gmail.com`}
+                            >joakimkartveit@gmail.com </a></li>
+                    </ul>
+                }
+            />
 
             <Footer/>
         </main>
